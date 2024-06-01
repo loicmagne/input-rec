6e6f31ce861f805ccf61a28f2c6bbb39fb0dc7a4
lm <loic.magne@outlook.com>
2024-06-01 15:00:53 +0200

    setup

--- src/main.cpp
+++ src/main.cpp
@@ -24,12 +24,11 @@ OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
 bool obs_module_load(void)
 {
-	obs_log(LOG_INFO, "plugin loaded successfully (version %s)",
-		PLUGIN_VERSION);
+	obs_log(LOG_INFO, "input-rec (version %s) loaded successfully", PLUGIN_VERSION);
 	return true;
 }
 
 void obs_module_unload(void)
 {
-	obs_log(LOG_INFO, "plugin unloaded");
+	obs_log(LOG_INFO, "input-rec unloaded");
 }
